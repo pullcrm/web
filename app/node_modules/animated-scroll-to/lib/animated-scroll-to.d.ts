b2778d05c97abc1cb97921c0d676d6ce63483aac
tsaryk <tsaryk@uteka.ru>
2022-06-03 22:50:38 +0300

    Implement ssr version

---
+++ app/node_modules/animated-scroll-to/lib/animated-scroll-to.d.ts
@@ -0,0 +1,16 @@
+export declare type TCoords = [number | null | undefined, number | null | undefined];
+export interface IOptions {
+    cancelOnUserAction?: boolean;
+    easing?: (t: number) => number;
+    elementToScroll?: Element | Window;
+    horizontalOffset?: number;
+    maxDuration?: number;
+    minDuration?: number;
+    passive?: boolean;
+    speed?: number;
+    verticalOffset?: number;
+}
+declare function animateScrollTo(y: number, userOptions?: IOptions): Promise<boolean>;
+declare function animateScrollTo(coords: TCoords, userOptions?: IOptions): Promise<boolean>;
+declare function animateScrollTo(scrollToElement: Element, userOptions?: IOptions): Promise<boolean>;
+export default animateScrollTo;
