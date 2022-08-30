5f11933d6206a8685d3ba2cb70a2aaea406ac6af
b_tsaryk <b.tsaryk@keepsolid.com>
2022-08-30 23:29:35 +0300

    Update blog styles

--- src/ui/Container.vue
+++ src/ui/Container.vue
@@ -1,8 +1,19 @@
 <script lang="ts" setup>
+defineProps({
+  narrow: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
 
 <template>
-  <div class="ui-container">
+  <div
+    class="ui-container"
+    :class="{
+      'ui-container__narrow': narrow,
+    }"
+  >
     <slot />
   </div>
 </template>
@@ -12,6 +23,10 @@
   @include ui-container;
 
   // @TODO: Refactor
-  max-width: 968px !important;
+  max-width: 980px !important;
+
+  &__narrow {
+    @include ui-container-narrow;
+  }
 }
 </style>
